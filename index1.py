cd2d5edde84036e3af6421c134e895debd2ea6c2
Parvati007 <parvati000777@gmail.com>
2025-08-04 01:09:28 +0530

    first commit

---
+++ index1.py
@@ -0,0 +1,39 @@
+#Writinf first program in python
+#print("Hello World!!")
+#print("My name is PARVATI.", "My age is 21.")
+#print(25+36)
+
+#Variables
+# name="PARVATI"
+# age=21
+# price=25.99
+# old=False
+# z=None
+# print(name,age,price)
+
+#Data Types
+# print(type(name))  #str
+# print(type(age))   #int
+# print(type(price)) #float
+# print(type(old))   #bool
+# print(type(a))     #NoneType
+
+#print sum of two numbers
+# a=2
+# b=5
+# sum=a+b
+# print(sum)
+
+#division 
+A,B=1,2
+C=A/B
+print(C)   #0.5
+
+#integer division
+a,b=1,2
+c=a//b
+print(c)   #0
+
+x,y=1.5,3
+z=x//y
+print(z)   #0.0
