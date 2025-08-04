6894ddfc2daf562bfbdc908d6c49565114ecc8e1
Parvati007 <parvati000777@gmail.com>
2025-08-05 01:20:39 +0530

    updated

--- index1.py
+++ index1.py
@@ -51,3 +51,117 @@
 #6. Integer division(//) with float and int will give int but displayed as float.
 #7. Integer divison with int and int result in int also.
 #8. remainder is negative when the denominator is negative.
+
+#Taking input from the user in python
+# name=input("name is : ")
+# age=int(input("age is : "))
+# price=float(input("price is : "))
+# print("my name is ",name,".I am",age,"years old.")
+
+#Conditional statements
+
+#traffic lights code
+# light=input("light is : ")
+# if(light == "red"):
+#     print("stop")
+# elif(light == "yellow"):
+#     print("look")
+# elif(light == "green"):
+#     print("go")
+# else:
+#     print("light is broken")   
+
+#grades of students
+# marks=int(input("marks is : "))
+# if(marks>=90):
+#     print("A")
+# elif(marks>=80 and marks<90):
+#     print("B")
+# elif(marks>=70 and marks<80):
+#     print("C")
+# else:
+#     print("D")  
+
+
+#Ternary operator
+# food = input("food is : ")
+# eat ="yes" if food=="cake" else "no"
+# print("Eat?", eat)
+
+
+# drink = input("drink is : ")
+# print("colddrink") if drink=="fanta"or drink=="coca cola" else print("no drinks")
+
+#Clever-If
+#var = (false , true) [condition]
+# age= int(input("age is : "))
+# vote = ("yes","no") [age<18]
+# print("vote?", vote)
+
+# sal=float(input("salary is : "))
+# tax=sal*(0.1,0.2) [sal>50000]
+# print("tax is : " ,tax)
+
+#Arithmetic operators
+# a=5
+# b=2
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+
+#Comparison operator (boolean)
+# a=10
+# b=20
+# print(a==b) #False
+# print(a!=b) #True
+# print(a>b)  #False
+# print(a<b)  #True
+# print(a>=b) #False
+# print(a<=b) #True
+
+#Assignment operator
+# num = 10
+# num += 10
+# print(num)
+# num -= 10
+# print(num)
+# num *= 10
+# print(num)
+# num /= 10
+# print(num)
+# num %= 10
+# print(num)
+# num **= 10
+# print(num)
+
+#Logical operator (boolean)
+# a=50
+# b=20
+# print(not False)  #True
+# print(not (a>b))  #False
+
+# val1=True
+# val2=True
+
+# print("AND operation : ", val1 and val2)
+# print("OR operation : ",val1 or val2)
+
+#Type conversion.....automatically
+# a=10
+# b=100.0
+# print(a+b)  #110.0
+
+#Type casting.......manually
+# a=int("2")
+# b=10
+# print(a+b)          #12
+
+# a=int("2")
+# b=15.7
+# print(a+b)           #17.7
+
+
+
