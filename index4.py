83171893243ff22ae5d1cbc4e8bb2cd8be387b8d
Parvati007 <parvati000777@gmail.com>
2025-08-10 00:11:00 +0530

    updated

--- index4.py
+++ index4.py
@@ -3,17 +3,36 @@
 #Dictionaries are used to store data values i key : values pairs
 #unordered, mutable(changeable), don't allow duplicates keys
 
-dict={
-    "name":"parvati",
-    "cgpa":8,
-    "marks":[98,89,95],
-}
+# dict={
+#     "name":"parvati",
+#     "cgpa":8,
+#     "marks":[98,89,95],
+# }
 
-dict["name"]="lakshmi"   #possible
-print(dict)
+# dict["name"]="lakshmi"   #possible
+# print(dict)
 
-#null dictionary
-null_dict={}
-null_dict["name"]="paru"
-print(null_dict)
+# #null dictionary
+# null_dict={}
+# null_dict["name"]="paru"
+# print(null_dict)
+
+#Nested dictionary is possible in python
+# student ={
+#     "name":"parvati",
+#     "subjects": {
+#         "phy":98,
+#         "chem":99,
+#     }
+# } 
+
+# print(student["subjects"]["phy"])  #98
+
+#DICTIONARY METHODS
+#1. dict.keys()
+#2. dict.values()
+#3. dict.items()
+#4. dict.get("name")
+#5. dict.update({"city":"jaipur"})
+
 
