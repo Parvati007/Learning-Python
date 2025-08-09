c750de3452d49839df65b8ba227f1edd95db23c0
Parvati007 <parvati000777@gmail.com>
2025-08-10 01:06:01 +0530

    day5

--- index4.py
+++ index4.py
@@ -35,4 +35,51 @@
 #4. dict.get("name")
 #5. dict.update({"city":"jaipur"})
 
+#--------------------------------------------------
+#SET:-set is the collection of unordered items
+#each element in the set must be unique and immutable
+
+#NOTE: set is mutable.......set ki values are immutable
+
+num={1,2,3,4}
+set2={1,2,2,2} #repeated elements stored only once....{1,2}
+null_set=set()  #empty set syntax
+
+#SET METHODS
+#1. set.add(element)
+#2. set.remove(element)
+#3. set.clear()
+#4. set.pop()
+#5. set.union(set2)
+#6. set.intersection(set2)
+
+# WAPP to store values in dict
+dictionary={
+    "cat":"a small animal",
+    "table":[" a piece of furniture","list of facts"],
+}
+print(dictionary)
+
+#WAP 1 subject....1 classroom
+subjects={
+    "python","java","c++","python","c","c++","java","javascript"
+}
+print(len(subjects))    #5
+
+#WAP enter 3 subjects marks store them in dictionary
+marks={}
+x=int(input("enter phy : "))
+marks.update({"phy ": x})
+x=int(input("enter chem : "))
+marks.update({"chem ": x})
+x=int(input("enter math : "))
+marks.update({"math ": x})
+print(marks)
+
+#WAP to store 9 and 9.0 separate in set
+values={9,"9.0"}
+print(values)
+
+val={("float",9.0),("int",9)}
+print(val)
 
