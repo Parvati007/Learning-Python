36d3cfc44977c299d6ee1ca189826ca88baa1495
Parvati007 <parvati000777@gmail.com>
2025-08-09 22:01:30 +0530

    updated

---
+++ index4.py
@@ -0,0 +1,17 @@
+#DICTIONARY AND SET
+#Dictionaries are used to store data values i key : values pairs
+#unordered, mutable(changeable), don't allow duplicates keys
+
+dict={
+    "name":"parvati",
+    "cgpa":8,
+    "marks":[98,89,95],
+}
+
+dict["name"]="lakshmi"   #possible
+print(dict)
+
+#null dictionary
+null_dict={}
+null_dict["name"]="paru"
+print(null_dict)
