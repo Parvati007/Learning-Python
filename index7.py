420e7e97de0b78a07c6c4eaef55da40f1a5cad7b
Parvati007 <parvati000777@gmail.com>
2025-08-12 01:01:33 +0530

    updated

--- index7.py
+++ index7.py
@@ -6,4 +6,13 @@ def show(n):
         return
     print(n)
     show(n-1)
-show(5)    
+show(5)                           # 5 4 3 2 1
+
+
+def fact(n):
+    if(n==1 or n==0):
+        return 1
+    return fact(n-1)*n
+print(fact(6))                 #720
+
+#--------------------------------------------------------------------------
