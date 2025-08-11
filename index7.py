2b017518d5f0073e829b708710f05fac42abc203
Parvati007 <parvati000777@gmail.com>
2025-08-12 01:05:25 +0530

    updated

--- index7.py
+++ index7.py
@@ -16,3 +16,21 @@ def fact(n):
 print(fact(6))                 #720
 
 #--------------------------------------------------------------------------
+#Write a recursive function to calculate the sum of first n natural numbers
+def cal_sum(n):
+    if(n==0):
+        return 0
+    return cal_sum(n-1)+n
+sum=cal_sum(5)
+print(sum)                    #15
+
+#---------------------------------------------------------------------------
+#write a recursive function to print all elements in a list 
+def print_list(list,idx=0):
+    if(idx==len(list)):
+        return
+    print( list[idx])
+    print_list(list,idx+1)
+
+fruits=["apple","litchi","banana","mango"]
+print_list(fruits)    
