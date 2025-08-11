a2a8753ced13131493e7459cb977764961d7e4a0
Parvati007 <parvati000777@gmail.com>
2025-08-11 23:18:28 +0530

    updated

---
+++ index6.py
@@ -0,0 +1,25 @@
+#FUNCTIONS:-
+#block of statement that perform a specific task
+
+def cal_sum(a,b):    #function definition...parameters
+    return a+b
+
+output =cal_sum(1,2)       #function call...arguments
+print(output)
+
+#-------------------------------------------------------
+def print_hello():
+    print("hello world")
+
+#print_hello()           #hello world
+out=print_hello()
+print(out)               #None
+
+#------------------------------------------------------
+def calc_avg(a,b,c):
+    sum=a+b+c
+    avg=sum/3
+    print(avg)
+    return avg
+
+calc_avg(1,2,3)         #2.0
