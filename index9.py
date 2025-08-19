0658c623f3ef72a95fe4e197cfc96dcb48098598
Parvati007 <parvati000777@gmail.com>
2025-08-19 23:45:26 +0530

    updated

--- index9.py
+++ index9.py
@@ -1,1 +1,6 @@
-#OOPS( object oriented programming) IN PYTHON
+#OOPS (object oriented programming) IN PYTHON
+# to map with real world scenarios we started using objects in code.this is called oop
+
+#CLASS AND OBJECTS
+#class is blueprint for creating objects
+#object is instance of class
