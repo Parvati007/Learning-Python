137a846396fe445600f323bf69e6807b83d25b40
Parvati007 <parvati000777@gmail.com>
2025-08-20 01:07:49 +0530

    updated

--- index10.py
+++ index10.py
@@ -1,3 +1,6 @@
 #OOPS IN PYTHON
 
 #ABSTRACTION:- hiding the implementation details of class and only showing the essential features to the user.
+
+#ENCAPSULATION:- wrapping data and functions into a single unit(objects)
+
