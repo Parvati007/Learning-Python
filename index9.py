934d0c83907ba939b1ae360e30614528c9090b4b
Parvati007 <parvati000777@gmail.com>
2025-08-20 00:04:24 +0530

    updated

--- index9.py
+++ index9.py
@@ -1,21 +1,29 @@
 #OOPS (object oriented programming) IN PYTHON
 #to map with real world scenarios we started using objects in code.this is called oop
 
+#----------------------------------------------------------------------------------
 #CLASS AND OBJECTS
 #class is blueprint for creating objects
 #object is instance of class
 
 #example :-
-class Student:
-    name="parvati"
+# class Student:
+#     name="parvati"
 
-s1=Student()
-print(s1.name)
+# s1=Student()
+# print(s1.name)
 
-class Car:
-    brand="BMW"
-    color="green"
+# class Car:
+#     brand="BMW"
+#     color="green"
 
-car1=Car()
-print(car1.brand)
-print(car1.color)    
+# car1=Car()
+# print(car1.brand)
+# print(car1.color)   
+#------------------------------------------------------
+
+#CONSTRUCTOR  __init__ function
+# all class have function called __init__ which is always executed when the object is being initiated
+#
+#example:-
+  
