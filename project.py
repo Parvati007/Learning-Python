ce65217b5c3b8c9228ffb4f2eb45332508aabded
Parvati007 <parvati000777@gmail.com>
2025-08-28 22:07:54 +0530

    updated

--- project.py
+++ project.py
@@ -2,15 +2,9 @@
 #some short and clear benefits of Python:
 
 # Easy to Learn & Readable – Simple syntax close to English.
-
 # Versatile – Used in web development, AI/ML, data science, automation, etc.
-
 # Large Community Support – Huge libraries, frameworks, and active community.
-
 # Cross-Platform – Works on Windows, macOS, Linux without major changes.
-
 # Rapid Development – Speeds up prototyping and project completion.
-
 # Strong Library Ecosystem – NumPy, Pandas, TensorFlow, Django, Flask, etc.
-
 # High Demand – Popular in industry, increasing career opportunities.
