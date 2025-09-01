d7071f02c11de6600ad0560302ef6162f801013d
Parvati007 <parvati000777@gmail.com>
2025-09-01 19:28:41 +0530

    updated

--- project.py
+++ project.py
@@ -8,3 +8,20 @@
 # Strong Library Ecosystem – NumPy, Pandas, TensorFlow, Django, Flask, etc.
 # High Demand – Popular in industry, increasing career opportunities.
 
+#GUESS THE NUMBER
+import random
+target =random.randint(1,100)
+while(True):
+    userChoice=input("Guess the target or Quit: ")
+    if(userChoice=="Quit"):
+        break
+    userChoice=int(userChoice)
+    if(userChoice==target):
+        print("Success : Correct Guess!! ")
+        break
+    elif(userChoice<target):
+        print("your number was too small.Take a bigger guess.")
+    else:
+        print('your number was too big.Take a smaller guess.')
+print('--------------------GAME OVER-------------------')            
+
