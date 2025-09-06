2bf91edafe716d170f776f1f0de03da7f28b520e
Parvati007 <parvati000777@gmail.com>
2025-09-06 20:17:28 +0530

    django

--- django1.py
+++ django1.py
@@ -1,1 +1,26 @@
 #DJANGO
+#django is free and open source web application framewoork written in python
+#django offers a big collection of modules which you can use in your own projects
+#save a lot time for developers
+
+#INSTALLING DJNAGO
+#pip install django
+
+#CREATE PROJECT
+#djnago-admin startproject projectName
+
+#CREATE APP
+#python manage.py startapp appname
+
+#STARTING SERVER
+#python manage.py runserver
+
+#---------------------------------------------------------------------------------------
+#MVT Architecture of django...........software design pattern
+#Model:- DATABASE
+#View:-  LOGIC
+#Template:- PRESENTATION
+
+#user----------django-----url----view------(1)model---(2)template
+#View is used to execute the business logic and interact with a model to carry data and render templates
+#there is no separate controllers
